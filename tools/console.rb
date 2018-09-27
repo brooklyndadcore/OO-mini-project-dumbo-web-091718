0763bd76098e0256475181cab02b5884e5e3dfc4
brooklyndadcore <scott.waring@gmail.com>
2018-09-27 15:08:46 -0400

    further class creation and refactoring- including allergens, ingredients, recipe, recipeingredients, and users

--- tools/console.rb
+++ tools/console.rb
@@ -1,3 +1,16 @@
 require_relative '../config/environment.rb'
 #
+
+
+
+bill = User.new("bill")
+tood = User.new("tood")
+steve = User.new("steve")
+matt = User.new("matt")
+bill = User.new("bill")
+jenny = User.new("jenny")
+sarah = User.new("sarah")
+p bill
+
+
 binding.pry
